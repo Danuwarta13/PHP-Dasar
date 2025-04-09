87054e9da48e816fcb7ae3614b6bb5b13fa820be
Danuwarta13 <danujr650@gmail.com>
2025-04-09 08:56:37 +0700

    Pertemuan 3 -Pengulangan & Pengkondisian

--- Pertemuan-3/cobaPengulanagan.php
+++ Pertemuan-3/cobaPengulanagan.php
@@ -5,20 +5,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 1</title>
+    <style>
+    .warna-baris {
+        background-color: lightblue;
+    }
+
+    .warna-kolom {
+        background-color: lightgreen;
+    }
+    </style>
 </head>
 
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
+        <?php // Cara pertama 
+            // for ($b = 1; $b <= 3; $b++) {
+            //     echo "<tr>";
+            //     for ($k=1; $k <= 5; $k++) { 
+            //         echo "<td>$b.$k </td>";
+            //     }
+            //     echo "</tr>";
+            // }
+        ?>
+
+        <!-- Cara Kedua -->
+        <?php for ($b=1; $b <= 5 ; $b++) : ?>
+        <?php if ($b % 2 == 1) : ?>
+        <tr class="warna-baris">
+            <?php else : ?>
         <tr>
-            <td>1.1</td>
-            <td>1.2</td>
-            <td>1.3</td>
+            <?php endif; ?>
+            <?php for ($k= 1; $k <= 5 ; $k++) : ?>
+            <!-- menggnuakan ternary -->
+            <?php echo $k %2 == 1 ? "<td class='warna-kolom'>$k.$b</td>" : "<td>$k.$b</td>" ?>
+            <?php endfor; ?>
         </tr>
-        <tr>
-            <td>1.1</td>
-            <td>1.2</td>
-            <td>1.3</td>
-        </tr>
+        <?php endfor; ?>
+
+
     </table>
 </body>
 
---
+++ Pertemuan-3/latihan2.php
@@ -0,0 +1,19 @@
+<?php
+    // Pengkodisian / Percabangan
+    // 1. if, else
+    // 2. if, else if, else
+    // 3. ternary operator
+    // 4. switch case
+    
+    $x = 20;
+    if ( $x < 20) {
+        echo "Benar <br>";
+    } elseif ($x == 20) {
+        echo "Nilai nya 20 Brok <br>";
+    } 
+    else {
+        echo "Salah <br>";
+    }
+
+
+?>
